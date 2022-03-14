50e42bf17eaae68df0ac9cdb44b3bc6f6f2c46da
Artem <artem@Users-MacBook-Pro.local>
2022-03-14 17:45:09 +0200

    styles for profil

--- src/components/SocialProfile/SocialProfile.js
+++ src/components/SocialProfile/SocialProfile.js
@@ -5,28 +5,41 @@ import s from './SocialProfile.module.css';
 export default function SocialProfile(props) {
   const { username, tag, location, avatar, stats } = props;
   return (
-    <div className={s.profile}>
-      <div className={s.description}>
-        <img src={avatar} alt={username} className={s.avatar} />
-        <p className={s.name}>{username}</p>
-        <p className={s.tag}>{tag}</p>
-        <p className={s.location}>{location}</p>
-      </div>
+    <div className={s.layot}>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <img src={avatar} alt={username} className={s.avatar} />
+          <p className={s.name}>{username}</p>
+          <p className={s.tag}>@{tag}</p>
+          <p className={s.location}>{location}</p>
+        </div>
 
-      <ul className={s.stats}>
-        <li>
-          <span className={s.label}>Followers</span>
-          <span className={s.quantity}>{stats.followers}</span>
-        </li>
-        <li>
-          <span className={s.label}>Views</span>
-          <span className={s.quantity}>{stats.views}</span>
-        </li>
-        <li>
-          <span className={s.label}>Likes</span>
-          <span className={s.quantity}>{stats.likes}</span>
-        </li>
-      </ul>
+        <ul className={s.stats}>
+          <li>
+            <span className={s.label}>Followers</span>
+            <span className={s.quantity}>{stats.followers}</span>
+          </li>
+          <li>
+            <span className={s.label}>Views</span>
+            <span className={s.quantity}>{stats.views}</span>
+          </li>
+          <li>
+            <span className={s.label}>Likes</span>
+            <span className={s.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
+SocialProfile.defaultProps = {
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+};
+
+SocialProfile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number.isRequired).isRequired,
+};
