a80aa6e23008e35e7d44972dca9b2a49a42f3d81
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-03-17 17:59:24 +0200

    friendlist

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -2,15 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Friend.module.css';
 
-export default function Friend({}) {
+export default function Friend({ props }) {
+  const { avatar, name, isOnline } = props;
   return (
-    <li class="item">
-      <span class="status"></span>
-      <img class="avatar" src="" alt="User avatar" width="48" />
-      <p class="name"></p>
+    <li className={s.item}>
+      <span className={isOnline ? s.online : s.offline}>{isOnline}</span>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
     </li>
   );
 }
+
+Friend.propTypes = {
+  props: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }).isRequired,
+};
 
 /* "avatar": "https://cdn-icons-png.flaticon.com/512/1623/1623681.png",
 "name": "Ajax",
--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -1,1 +1,22 @@
-<ul class="friend-list"></ul>;
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './FriendsList.module.css';
+import Friend from '../Friend/Friend';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(friend => (
+        <Friend props={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
