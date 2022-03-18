6bd17caf15aa4e44fa2584aea6b10a2a70557790
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-03-18 14:33:58 +0200

    transaction finish

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-      <FriendList friends={friends} /> */}
+      <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
   );
--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Transactions.module.css';
+import { type } from '@testing-library/user-event/dist/type';
 
 export default function TransactionHistory({ items }) {
   return (
     <table className={s.transaction}>
       <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+        <tr className={s.theader}>
+          <th className={s.head}>Type</th>
+          <th className={s.head}>Amount</th>
+          <th className={s.head}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
+        {items.map(({ id, type, amount, currency }, idx) => (
+          <tr className={idx % 2 === 0 ? s.even : s.odd} key={id}>
+            <td className={s.type}>{type /* .toUpperCase() */}</td>
+            <td className={s.amount}>{amount}</td>
+            <td className={s.currency}>{currency}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
@@ -29,9 +32,12 @@ export default function TransactionHistory({ items }) {
 "amount": "828.62",
 "currency": "UGX" */
 TransactionHistory.propTypes = {
-  friends: PropTypes.arrayOf(
+  items: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
     }).isRequired,
   ).isRequired,
 };
